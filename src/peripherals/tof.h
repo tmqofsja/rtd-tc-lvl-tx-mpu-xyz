3fe63a478fad2bbe1c13b27ff94e79da5aef00d3
wook <tmqofsja.naver.com>
2024-08-19 19:54:55 +0900

    while(!sensor.tofReady) {     tcaselect(2);//Adafruit TCA9548A I2C Multiplexer module     sensor.tofReady = tof_sensor.begin(0x29, false, &Wire, Adafruit_VL53L0X::VL53L0X_SENSE_HIGH_ACCURACY);   }

--- src/peripherals/tof.h
+++ src/peripherals/tof.h
@@ -33,10 +33,8 @@ TOF::TOF() {}
 
 void TOF::init(SensorState& sensor) {
   #ifdef TOF_VL53L0X
-
-  tcaselect(2);//Adafruit TCA9548A I2C Multiplexer module
-  
   while(!sensor.tofReady) {
+    tcaselect(2);//Adafruit TCA9548A I2C Multiplexer module
     sensor.tofReady = tof_sensor.begin(0x29, false, &Wire, Adafruit_VL53L0X::VL53L0X_SENSE_HIGH_ACCURACY);
   }
   tof_sensor.startRangeContinuous();
